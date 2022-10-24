98d20ec505b3b28fc242509c4174a2c86eb79163
Ananyobrata Pal <ananyo141@gmail.com>
2022-10-24 16:23:29 +0530

    add Experience component update site title change about picture layout to the left column (with flex)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Experience />
       <Projects />
       <Contact />
     </div>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,31 +5,31 @@ const About = () => {
   return (
     <div
       name="About"
-      className="flex flex-col gap-8 h-screen justify-center items-center text-gray-400"
+      className="flex h-screen text-gray-300 gap-10 items-center justify-center"
     >
-      <div name="About String" className="grid grid-cols-2 gap-4">
-        <h3 className="text-3xl font-bold border-b-4 pb-2 text-gray-200 border-pink-500">
+      <div className="flex flex-col items-end">
+        <h3 className="text-3xl mb-10 font-bold border-b-4 pb-2 border-pink-500">
           About
         </h3>
-        <div></div>
-      </div>
-      <div className="grid grid-cols-2 gap-4">
-        <p className="text-2xl max-w-lg font-bold text-gray-200 text-right">
+        <p className="text-2xl max-w-md font-bold text-right">
           Hey, I am Ananyobrata Pal, nice to meet you! You can contact me, or
           just have a look around!
         </p>
-        <p className="max-w-lg text-gray-300">
-          I am a CSE Undergrad diving into the world of technology, interested
-          in building full stack apps. An Open Source advocate obsessed with
-          ArchLinux and (N)vim. I am passionate about building excellent
-          software that improves the lives of those around me.
-        </p>
+        <img
+          src={facePhoto}
+          className="w-60 h-60 rounded-xl block mt-12 mr-4 outline outline-offset-8"
+          alt="Image of Me"
+        />
       </div>
-      <img
-        src={facePhoto}
-        className="w-60 h-60 rounded-full block outline outline-offset-8"
-        alt="Image of Me"
-      />
+      <p className="max-w-sm lg:max-w-md font-light text-lg text-gray-400">
+        I am a CSE Undergrad diving into the world of technology, interested in
+        building full stack apps. An Open Source advocate obsessed with
+        ArchLinux and (N)vim. I am passionate about building excellent software
+        that improves the lives of those around me. I specialize in creating
+        software for clients ranging from individuals and small-businesses all
+        the way to large enterprise corporations. What would you do if you had a
+        software expert available at your fingertips?
+      </p>
     </div>
   );
 };
