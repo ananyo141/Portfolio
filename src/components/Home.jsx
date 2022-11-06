db49aa3e4eb6c1227c28fc6e701d56728ed86de5
Ananyobrata Pal <ananyo141@gmail.com>
2022-11-06 12:12:59 +0530

    add favicon and resume.pdf

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 
+import resumepdf from "../assets/Resume.pdf";
+
 const Home = () => {
   return (
-    <div name="Home" className="flex items-center justify-center h-screen max-w-lg lg:max-w-full">
+    <div
+      name="Home"
+      className="flex items-center justify-center h-screen max-w-lg lg:max-w-full"
+    >
       <div className="space-y-6 max-w-4xl">
         <h4 className="text-xl -mb-6 text-pink-400">Hello World, my name is</h4>
         <h1 className="text-6xl font-bold text-[#ccd6f6]">Ananyobrata Pal</h1>
-        <h1 className="text-6xl font-bold text-[#9b7c58]">I'm a Full Stack Developer.</h1>
+        <h1 className="text-6xl font-bold text-[#9b7c58]">
+          I'm a Full Stack Developer.
+        </h1>
         <h4 className="text-xl text-[#9b7c58]">
           I am a full stack developer specializing in building and designing
           full featured web and mobile apps. An ardent linux lover, I also am a
           competitive programmer.
         </h4>
-        <button className="px-4 py-2 border border-[#ccd6f6] text-[#ccd6f6]">Resume</button>
+        <button className="px-4 py-2 border border-[#ccd6f6] text-[#ccd6f6]">
+          <a href={resumepdf} target="_blank" rel="noreferrer">
+            Resume
+          </a>
+        </button>
       </div>
     </div>
   );
