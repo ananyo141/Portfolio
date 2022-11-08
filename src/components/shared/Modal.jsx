12164b2ad601de467ec67e1c4b8ed2efd5e242c6
Ananyobrata Pal <ananyo141@gmail.com>
2022-11-08 18:21:53 +0530

    separate Animations file use flip animation in Modal.jsx

--- src/components/shared/Modal.jsx
+++ src/components/shared/Modal.jsx
@@ -1,34 +1,16 @@
 import Backdrop from "./Backdrop";
 import { motion } from "framer-motion";
 
+import { Flip } from "./Animations";
+
 const Modal = ({ handleClose, text }) => {
-  const dropIn = {
-    hidden: {
-      y: "-100vh",
-      opacity: 0,
-    },
-    visible: {
-      y: "0",
-      opacity: 1,
-      transition: {
-        duration: 0.1,
-        type: "spring",
-        damping: 25,
-        stiffness: 500,
-      },
-    },
-    exit: {
-      y: "100vh",
-      opacity: 0,
-    },
-  };
   return (
     <Backdrop onClick={handleClose}>
       <motion.div
         drag
         onClick={(e) => e.stopPropagation()}
         className="mx-auto my-auto flex h-1/2 w-1/2 flex-col items-center rounded-xl pr-4"
-        variants={dropIn}
+        variants={Flip}
         initial="hidden"
         animate="visible"
         exit="exit"
