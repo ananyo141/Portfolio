80cc34588c874bbc56a5a9682af00aaf2109b2d0
Ananyobrata Pal <ananyo141@gmail.com>
2022-11-17 21:24:51 +0530

    remove swiper installation

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,4 @@
 import React from "react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Swiper Styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
 
 import ProjectCard from "../components/ProjectCard";
 
