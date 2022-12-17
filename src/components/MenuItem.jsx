fa9a4a7329003d9da76b9829d912b92492a0fe8b
Ananyobrata Pal <ananyo141@gmail.com>
2022-12-17 14:50:28 +0530

    Add Navbar functionality feat:  - add links to mobile menu  - for smaller screens view menu in full screen
    chore:
     - rearrange imports
     - refactor code navitems for avoiding code redundancy with loops

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-scroll";
 
-const MenuItem = () => {
+const MenuItem = ({ icon, title, linkname }) => {
   return (
     <li className="mb-5 flex cursor-pointer items-center space-x-6">
       <span className="h-[45px] w-[45px] rounded-full border-2 border-blue-300"></span>
-      <span className="h-8 flex-1 rounded-lg border-2 border-blue-400"></span>
+      <Link className="h-8 flex-1 rounded-lg border-2 border-blue-400 px-4" activeClass="active" to={linkname} smooth={true} duration={500}>
+        <span className="uppercase">
+          {title}
+        </span>
+      </Link>
     </li>
   );
 };
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,47 +1,39 @@
 import React, { useState } from "react";
+import { Link } from "react-scroll";
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
+import { motion } from "framer-motion";
 
 import logo from "../assets/logo.png";
 import MenuItem from "../components/MenuItem";
 
-import { Link } from "react-scroll";
-import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
+// Navigation entries; have save section names
+const NavItems = [
+  "Home",
+  "About",
+  "Skills",
+  "Experience",
+  "Projects",
+  "Testimonials",
+  "Contact",
+];
 
+// Top navigation bar for larger displays
 const Navtopbar = () => {
   return (
     <nav className="fixed top-0 left-0 z-50 hidden w-full items-center justify-between py-2 px-2 backdrop-blur-md md:flex">
       <img src={logo} className="ml-4 w-40" alt="Logo" />
       <ul className="flex justify-between pr-8 text-lg text-slate-300 sm:space-x-4 md:space-x-6 lg:space-x-12">
-        <Link activeClass="active" to="Home" smooth={true} duration={500}>
-          <li className="cursor-pointer">Home</li>
-        </Link>
-        <Link activeClass="active" to="About" smooth={true} duration={500}>
-          <li className="cursor-pointer">About</li>
-        </Link>
-        <Link activeClass="active" to="Skills" smooth={true} duration={500}>
-          <li className="cursor-pointer">Skills</li>
-        </Link>
-        <Link activeClass="active" to="Experience" smooth={true} duration={500}>
-          <li className="cursor-pointer">Experience</li>
-        </Link>
-        <Link activeClass="active" to="Projects" smooth={true} duration={500}>
-          <li className="cursor-pointer">Projects</li>
-        </Link>
-        <Link
-          activeClass="active"
-          to="Testimonials"
-          smooth={true}
-          duration={500}
-        >
-          <li className="cursor-pointer">Testimonials</li>
-        </Link>
-        <Link activeClass="active" to="Contact" smooth={true} duration={500}>
-          <li className="cursor-pointer">Contact</li>
-        </Link>
+        {NavItems.map((item) => (
+          <Link activeClass="active" to={item} smooth={true} duration={500}>
+            <li className="cursor-pointer">{item}</li>
+          </Link>
+        ))}
       </ul>
     </nav>
   );
 };
 
+// Side navigation bar for mobile view
 // https://www.youtube.com/watch?v=qppZVzJB8oA
 const Navsidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -50,26 +42,31 @@ const Navsidebar = () => {
   return (
     <>
       <RxHamburgerMenu
-        className="-ml-3 mt-2 scale-150 text-white cursor-pointer md:hidden"
+        className="fixed -ml-3 mt-2 scale-150 cursor-pointer text-white backdrop-blur md:hidden"
         onClick={toggleMenu}
       />
       {isOpen ? (
-        <nav className="fixed top-0 left-0 bottom-0 w-[300px] bg-white md:hidden">
+        <motion.nav className="fixed top-0 left-0 bottom-0 z-50 w-full bg-white sm:w-[300px] md:hidden">
           <RxCross1
             className="ml-7 mt-6 scale-150 cursor-pointer"
             onClick={toggleMenu}
           />
-          <ul className="fixed top-[100px] w-[260px] p-5">
-            {[...Array(5)].map((_, i) => (
-              <MenuItem key={i} />
-            ))}
-          </ul>
-        </nav>
+          <div className="flex justify-center">
+            <div>
+              <ul className="mt-5 w-full p-5 sm:w-[260px]">
+                {NavItems.map((item) => (
+                  <MenuItem icon={null} title={item} linkname={item} />
+                ))}
+              </ul>
+            </div>
+          </div>
+        </motion.nav>
       ) : null}
     </>
   );
 };
 
+// Main navbar with conditional rendering
 const Navbar = () => {
   return (
     <section name="Navbar">
